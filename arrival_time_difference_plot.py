7fc533672e0663eab551f79a14e0c647cb2ec83c
root <root@nf-server13.nf.cl.cam.ac.uk>
2019-02-14 18:07:34 +0000

    Add a graph showing the difference between two different pcap files

--- arrival_time_difference_plot.py
+++ arrival_time_difference_plot.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import process_txt
+import process_pcap
 import sys
 
 if __name__ == "__main__":
@@ -15,20 +16,18 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     first_file = args.first_file
-    clear_first_file = False
     second_file = args.second_file
-    clear_second_file = False
 
     if args.first_file.endswith('.pcap'):
-        clear_first_file = True
-        first_file = process_txt.create_txt_from_pcap(args.first_file)
+        first_times = np.array(process_pcap.extract_times(first_file))
+    else:
+        first_times = np.array(process_txt.extract_times(first_file))
 
     if args.second_file.endswith('.pcap'):
-        clear_second_file = True
-        second_file = process_txt.create_txt_from_pcap(args.second_file)
+        second_times = np.array(process_pcap.extract_times(second_file))
+    else:
+        second_times = np.array(process_txt.extract_times(second_file))
 
-    first_times = np.array(process_txt.extract_times(first_file))
-    second_times = np.array(process_txt.extract_times(second_file))
 
     if len(first_times) != len(second_times):
         print "Error: There are a different number of packets in each trace"
@@ -37,16 +36,15 @@ if __name__ == "__main__":
     # Now, go through each time and calculate the difference.
     # Plot that difference in a histogram.
     diffs = first_times - second_times
+    # Convert to ns:
+    diffs = diffs * (10 ** 9)
+    print "Plottiong ", len(diffs), "packets"
     print diffs
     plt.hist(diffs, bins=60)
     plt.title("Difference Between arrival times")
     plt.xlabel("Difference (ns)")
     plt.ylabel("Frequency")
+    plt.xlim([min(diffs), max(diffs)])
     plt.tight_layout()
     plt.savefig(os.path.basename(first_file) + '_diff_' + \
             os.path.basename(second_file) + '.eps')
-
-    if clear_first_file:
-        os.remove(first_file)
-    if clear_second_file:
-        os.remove(second_file)

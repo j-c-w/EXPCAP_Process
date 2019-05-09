9bb6dc1ec02f5441db83b577242fb271504a18d7
Jackson Woodruff <woodruff.jackson@gmail.com>
2019-05-09 16:28:43 +0100

    Add a --server flag

--- inter_arrival_distribution_graph.py
+++ inter_arrival_distribution_graph.py
@@ -10,7 +10,8 @@ import process_pcap
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file')
-    parser.add_argument('--bins', type=int, dest='bins', help="Number of bins", default=30)
+    parser.add_argument('--bins', type=int, dest='bins', help="Number of bins", default=1000)
+    parser.add_argument('--server', dest='server_ip', help="The IP address of the server", required=True)
     parser.add_argument('--keep-temps', dest='keep_temps', default=False, action='store_true', help="Keep temp files")
     # This is to avoid issues with tcpdump hanging.
     parser.add_argument('--packets', type=int, required=False,
@@ -21,25 +22,42 @@ if __name__ == "__main__":
 
     pcap_file = args.input_file
 
-    if pcap_file.endswith('.pcap'):
-        timestamp_deltas = process_pcap.extract_deltas(pcap_file)
-    elif pcap_file.endswith('.csv'):
-        timestamp_deltas = process_csv.extract_deltas(pcap_file)
-    else:
-        timestamp_deltas = process_txt.extract_deltas(pcap_file)
+    if pcap_file.endswith('.csv'):
+        timestamp_deltas_incoming = \
+            process_csv.extract_deltas(pcap_file, to_ip=args.server_ip)
+        timestamp_deltas_outgoing = \
+            process_csv.extract_deltas(pcap_file, from_ip=args.server_ip)
 
     # Convert to ns before starting:
-    for i in range(len(timestamp_deltas)):
-        timestamp_deltas[i] = 1000000000.0 * timestamp_deltas[i]
+    for i in range(len(timestamp_deltas_incoming)):
+        timestamp_deltas_incoming[i] = 1000000000.0 * timestamp_deltas_incoming[i]
+    for i in range(len(timestamp_deltas_outgoing)):
+        timestamp_deltas_outgoing[i] = 1000000000.0 * timestamp_deltas_outgoing[i]
 
-    range = [min(timestamp_deltas), max(timestamp_deltas)]
+    # Do the outgoing packets.
+    range = [min(timestamp_deltas_outgoing), max(timestamp_deltas_outgoing)]
     print "Range is ", range
-    print "Median is ", np.median(timestamp_deltas)
-    print "Deviation is ", np.std(timestamp_deltas)
-    timestamp_deltas = np.asarray(timestamp_deltas, dtype='float')
+    print "Median is ", np.median(timestamp_deltas_outgoing)
+    print "Deviation is ", np.std(timestamp_deltas_outgoing)
+    timestamp_deltas_outgoing = np.asarray(timestamp_deltas_outgoing, dtype='float')
 
-    plt.hist(timestamp_deltas, bins=args.bins)
+    plt.hist(timestamp_deltas_outgoing, bins=args.bins, cumlative=True,
+             histype='step', normed=True)
     plt.ylabel("Number of Packets")
     plt.xlabel("Inter-arrival time (ns)")
-    plt.savefig(pcap_file + '_interarrival.eps', format='eps')
-    print "Done! File is in ", pcap_file + '_interarrival.eps'
+    plt.savefig(pcap_file + '_outgoing_interarrival.eps', format='eps')
+    print "Done! File is in ", pcap_file + '_outgoing_interarrival.eps'
+
+    # Do the incoming packets.
+    range = [min(timestamp_deltas_incoming), max(timestamp_deltas_incoming)]
+    print "Incoming Range is ", range
+    print "Incoming Median is ", np.median(timestamp_deltas_incoming)
+    print "Incoming Deviation is ", np.std(timestamp_deltas_incoming)
+    timestamp_deltas_incoming = np.asarray(timestamp_deltas_incoming, dtype='float')
+
+    plt.hist(timestamp_deltas_incoming, bins=args.bins)
+    plt.ylabel("Number of Packets")
+    plt.xlabel("Inter-arrival time (ns)")
+    plt.title("Cumlative Distribution of Inter-Packet Gaps")
+    plt.savefig(pcap_file + '_incoming_interarrival.eps', format='eps')
+    print "Done! File is in ", pcap_file + '_incoming_interarrival.eps'

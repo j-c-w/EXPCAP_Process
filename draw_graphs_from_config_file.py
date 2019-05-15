d51820b3a6f0551227743e0e031a435445049876
Jackson Woodruff <woodruff.jackson@gmail.com>
2019-05-15 16:49:14 +0100

    Add a script that can draw the graphs from a similar config file

---
+++ draw_graphs_from_config_file.py
@@ -0,0 +1,103 @@
+import argparse
+import threading
+import shlex
+import arrival_time_difference_plot
+import bandwidth_cdf
+import bandwidth_distribution
+import bandwidth_through_time
+import flow_length_cdf
+import flow_size_cdf
+import inter_arrival_distribution_graph
+import interarrival_statistics
+import ipg_distribution_graph
+import microburst_analysis
+import packet_size_distribution_graph
+import packet_size_distribution_through_time
+import plot_arrival_times
+
+entry_semaphore = None
+
+
+def draw_graph(name, commands):
+    global entry_semaphore
+    entry_semaphore.acquire()
+    print "A thread has acquired the global semaphore and is starting work."
+    print "(Using ", name, ")"
+
+    try:
+        if name == "arrival_time_difference_plot.py":
+            arrival_time_difference_plot.main(commands)
+        elif name == "bandwidth_cdf.py":
+            bandwidth_cdf.main(commands)
+        elif name == "bandwidth_distribution.py":
+            bandwidth_distribution.main(commands)
+        elif name == "bandwidth_through_time.py":
+            bandwidth_through_time.main(commands)
+        elif name == "flow_length_cdf.py":
+            flow_length_cdf.main(commands)
+        elif name == "flow_size_cdf.py":
+            flow_size_cdf.main(commands)
+        elif name == "inter_arrival_distribution_graph.py":
+            inter_arrival_distribution_graph.main(commands)
+        elif name == "interarrival_statistics.py":
+            interarrival_statistics.main(commands)
+        elif name == "ipg_distribution_graph.py":
+            ipg_distribution_graph.main(commands)
+        elif name == "microburst_analysis.py":
+            microburst_analysis.main(commands)
+        elif name == "packet_size_distribution_graph.py":
+            packet_size_distribution_graph.main(commands)
+        elif name == "packet_size_distribution_through_time.py":
+            packet_size_distribution_through_time.main(commands)
+        elif name == "plot_arrival_times.py":
+            plot_arrival_times.main(commands)
+        else:
+            print "Error: Unkown filename", name
+    finally:
+        entry_semaphore.release()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--parallelism', dest='parallelism',
+                        default=1, required=False)
+    parser.add_argument('--dry-run', dest='dry_run',
+                        default=False, action='store_true')
+    parser.add_argument('config_file')
+
+    args = parser.parse_args()
+
+    entry_semaphore = threading.Semaphore(args.parallelism)
+    commands = []
+
+    with open(args.config_file) as f:
+        # Get all the lines.  Lines starting with a '#' are comments.
+
+        for line in f.readlines():
+            if not line.startswith('#'):
+                # The first CSV value is the plotting command.
+                command = line.split(',')[1]
+
+                # Splits the string using command line
+                # separators.
+                command_parts = shlex.split(command)
+                # The zeroeth element is just 'python'.
+                # The next element if the name of the file we need.
+                graph_name = command_parts[1]
+                command_parts = command_parts[2:]
+                commands.append((graph_name, command_parts))
+
+    if args.dry_run:
+        print commands
+    else:
+        threads = []
+        for (name, command) in commands:
+            threads.append(threading.Thread(target=draw_graph, args=[name, command]))
+        for thread in threads:
+            thread.start()
+        print "All threads started!"
+
+        for thread in threads:
+            thread.join()
+        print "All done!"

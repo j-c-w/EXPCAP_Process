65214ecd07c092fc4d8771bd82c98b3b47fcec74
Jackson Woodruff <woodruff.jackson@gmail.com>
2019-05-18 20:59:02 +0100

    No longer use integer ticks because those were causing problems. Also add log graphs

--- flow_size_cdf.py
+++ flow_size_cdf.py
@@ -22,6 +22,8 @@ def main(args):
     args = parser.parse_args(args)
     plt.figure(1)
     plt.clf()
+    plt.figure(2)
+    plt.clf()
 
     for pcap_file, label in args.input_files:
         if pcap_file.endswith('.csv'):
@@ -37,22 +39,50 @@ def main(args):
         min_lim = min(flow_sizes)
         max_lim = max(flow_sizes)
         small_diff = (min_lim + max_lim) / 10000.0
+
         bins = np.append(np.linspace(min_lim, max_lim + small_diff, 1000), np.inf)
+        plt.figure(1)
         plt.hist(flow_sizes, cumulative=True, bins=bins, histtype='step', normed=True, label=label)
 
+        no_zero_sizes = graph_utils.no_zeroes(flow_sizes)
+        if len(no_zero_sizes) > 0:
+            lim_min = min(no_zero_sizes)
+            lim_max = max(no_zero_sizes)
+
+            bins = graph_utils.get_logspace(lim_min, lim_max)
+            plt.figure(2)
+            plt.hist(no_zero_sizes, cumulative=True, bins=bins, histtype='step', normed=True, label=label)
+        else:
+            print "Hard warning!: There are no non-zero flow sizes"
+
     if args.title:
+        plt.figure(1)
+        plt.title(args.title)
+        plt.figure(2)
         plt.title(args.title)
 
     plt.figure(1)
     plt.xlabel("Flow Size (B)")
     plt.ylabel("CDF")
-    graph_utils.legend_bottom_right()
+    graph_utils.set_legend_below()
     graph_utils.set_yax_max_one()
-    graph_utils.set_integer_ticks()
     graph_utils.set_non_negative_axes()
     graph_utils.set_ticks()
-    plt.xticks(rotation=90)
+    plt.xticks()
     filename = args.output_name + '_flow_sizes.eps'
+    plt.savefig(filename)
+    print "Done! File is in ", filename
+
+    plt.figure(2)
+    plt.xlabel("Flow Size (B)")
+    plt.ylabel("CDF")
+    graph_utils.set_legend_below()
+    graph_utils.set_log_x()
+    graph_utils.set_yax_max_one()
+    graph_utils.set_non_negative_axes()
+    graph_utils.set_ticks()
+    plt.xticks()
+    filename = args.output_name + '_flow_sizes_log.eps'
     plt.savefig(filename)
     print "Done! File is in ", filename
 

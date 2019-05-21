d6284035d6bb3c5f072bf3fa9b21207901159682
Jackson Woodruff <woodruff.jackson@gmail.com>
2019-05-21 16:42:50 +0100

    Change arrival time difference plot to a CDF

--- arrival_time_difference_plot.py
+++ arrival_time_difference_plot.py
@@ -3,6 +3,7 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
+import graph_utils
 import os
 import process_csv
 import process_txt
@@ -49,10 +50,12 @@ def main(args):
     diffs = np.asarray(diffs, dtype='float')
     print "Plottiong ", len(diffs), "packets"
     print min(diffs), max(diffs)
-    plt.hist(diffs, bins=60)
-    plt.title("Difference Between arrival times")
+    bins = graph_utils.get_linspace(min(diffs), max(diffs))
+    plt.hist(diffs, cumulative=True, bins=bins, histtype='step', normed=True)
     plt.xlabel("Difference (ns)")
-    plt.ylabel("Frequency")
+    plt.ylabel("CDF")
+    graph_utils.set_yax_max_one()
+    graph_utils.set_ticks()
     plt.tight_layout()
     filename = os.path.basename(first_file) + '_diff_' + \
         os.path.basename(second_file) + '.eps'
